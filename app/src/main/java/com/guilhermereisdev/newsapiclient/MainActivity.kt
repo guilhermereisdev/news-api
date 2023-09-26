c1d3837ae9b67dba31f43b41d42bf7ed35637f50
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-09-26 04:20:26 -0300

    Adiciona a lista de notícias na tela inicial

--- app/src/main/java/com/guilhermereisdev/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/guilhermereisdev/newsapiclient/MainActivity.kt
@@ -2,11 +2,20 @@ package com.guilhermereisdev.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.guilhermereisdev.newsapiclient.databinding.ActivityMainBinding
+import com.guilhermereisdev.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.guilhermereisdev.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +28,7 @@ class MainActivity : AppCompatActivity() {
         binding.bnvNews.setupWithNavController(
             navController
         )
+
+        viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
